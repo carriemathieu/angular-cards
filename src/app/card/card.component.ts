0aabde8249396a11d2fbafe5c7ca7e8170c67a2d
Carrie Mathieu <carrieburks@gmail.com>
2021-08-01 12:03:42 -0400

    add properties to card component template

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   // selector - creates instance of component that matches the selector, adds to app-root element ('host element')
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  // instructs card component that it's going to receive these 4 properties
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() content = '';
+  @Input() username = '';
 
   constructor() { }
 
