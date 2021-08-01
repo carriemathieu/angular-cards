09e288eb5ad44ba20781867927a7d718dbe33557
Carrie Mathieu <carrieburks@gmail.com>
2021-08-01 11:11:10 -0400

    add comments to card component.ts

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector - creates instance of component that matches the selector, adds to app-root element ('host element')
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
